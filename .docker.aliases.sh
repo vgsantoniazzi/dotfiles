0572db219ba5ccfb67f634081ba687f9453eb826
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2022-05-10 20:45:57 -0300

    Add docker clean command

--- .docker.aliases.sh
+++ .docker.aliases.sh
@@ -4,10 +4,33 @@ alias dcr="docker-compose run --rm"
 
 alias k="kubectl"
 
-docker_clean(){
-  docker rm $(docker ps --all -q -f status=exited)
+docker__clean() {
+    echo "Stopping containers.."
+    result=$(docker ps -a -q)
+    [[ ! -z "$result" ]] && docker stop "$result"
+
+    echo "Removing containers.."
+    result=$(docker ps --all -q -f status=exited)
+    [[ ! -z "$result" ]] && docker rm result
+
+    echo "Removing images.."
+    for image in "$@"
+    do
+        echo "Removing image \"$image\""
+        [[ ! -z $(docker images -q "$image") ]] && docker rmi -f "$image"
+    done
+
+    echo "Removing dangling images.."
+    result=$(docker images -f "dangling=true" -q)
+    [[ ! -z "$result" ]] && docker rmi -f result
+    echo "Done!"
 }
 
-docker_stop() {
-  docker stop $(docker ps -a -q)
+docker() {
+  local cmdname=$1; shift
+  if type "docker__$cmdname" >/dev/null 2>&1; then
+    "docker__$cmdname" "$@"
+  else
+    command docker "$cmdname" "$@" # call the **real** docker command
+  fi
 }
